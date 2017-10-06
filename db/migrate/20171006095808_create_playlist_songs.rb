ac853e50dfe071c1d7787ee7dca2b06cf30e7e01
Mina Parnian <minaparnian66@gmail.com>
2017-10-06 21:42:07 +1100

    chillUp assosiation

---
+++ db/migrate/20171006095808_create_playlist_songs.rb
@@ -0,0 +1,8 @@
+class CreatePlaylistSongs < ActiveRecord::Migration[5.1]
+  def change
+    create_table :playlists_songs, :id => false do |t|
+      t.integer :playlist_id
+      t.integer :song_id
+    end
+  end
+end
