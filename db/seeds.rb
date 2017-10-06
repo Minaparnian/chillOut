5cb52c38cefe72773e525ea9b8a0e3668ef9525c
Mina Parnian <minaparnian66@gmail.com>
2017-10-06 23:04:26 +1100

    chillout seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,47 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+
+u1 = User.create :email => "mina.mina@ga.com"
+u2 = User.create :email => "saman.saman@ga.com"
+
+
+Song.destroy_all
+
+s1 = Song.create :name => "Crazy in love"
+s2 = Song.create :name => "Nada"
+
+
+Album.destroy_all
+
+a1 = Album.create :name => "Dangerously in love", :image => "https://upload.wikimedia.org/wikipedia/en/8/84/Dangerously_In_Love_Album%282003%29.png"
+a2 = Album.create :name => "El Dorado", :image => "https://upload.wikimedia.org/wikipedia/en/thumb/a/a7/Shakira_El_Dorado_cover.png/220px-Shakira_El_Dorado_cover.png"
+
+Artist.destroy_all
+
+r1 = Artist.create :name => "Beyonce"
+r2 = Artist.create :name => "Shakira"
+
+
+Playlist.destroy_all
+
+p1 = Playlist.create :name => "Viva Latino-Top 50"
+p2 = Playlist.create :name => "Best of 2017"
+
+
+
+
+####Assosciation##############################################
+
+
+a1.songs << s1
+a2.songs << s2
+
+r1.songs << s1
+r2.songs << s2
+
+
+p1.songs << s1 << s2
+p2.songs << s2
+
+
+u1.playlists << p1 << p2
+u2.playlists << p2
