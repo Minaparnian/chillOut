31c7bab94c60ed4b8f39ff909c431912050cc139
Mina Parnian <minaparnian66@gmail.com>
2017-10-07 19:46:05 +1100

    Search Artist

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  get 'users/index'
 
-  get 'users/new'
 
-  get 'users/edit'
+  root :to => 'pages#index'
+  resources :users
 
-  get 'users/show'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/login' => 'session#new'
+  post '/login' => 'session#create'
+  delete '/login' => 'session#destroy'
 end
