75cf70b7e14fede839b206b22740ad79911f44ad
Mina Parnian <minaparnian66@gmail.com>
2017-10-12 13:00:03 +1100

    audio visualasation

--- app/assets/javascripts/playlist.js
+++ app/assets/javascripts/playlist.js
@@ -1,33 +1,63 @@
-// $(function() {
-//
-//   $( make_draggable_and_droppable() );
-//
-//   function make_draggable_and_droppable() {
-//     $('.this_is_draggable').draggable({
-//       helper: 'clone',
-//       snap: '.this_is_droppable',
-//       cursor: 'move'
-//     });
-//
-//     $('.this_is_droppable').droppable( {
-//       drop: drop_track
-//     });
-//   }
-//
-//   function drop_article(event,ui) {
-//     $.ajax({
-//       type: 'POST',
-//       url:    '/links/',
-//       data: {link : {
-//         parent_id : this.id,
-//         child_id : ui.draggable.attr('id')
-//       }
-//     },
-//     dataType: 'script',
-//     remote: 'true',
-//     success: function(){
-//     }
-//   });
-//   }
-// //closing document ready tag
-// });
+window.onload = function() {
+console.log("loaded");
+  // set up audio context
+  var audioContext = (window.AudioContext || window.webkitAudioContext);
+  // create audio class
+  if (audioContext) {
+    // Web Audio API is available.
+    var audioAPI = new audioContext();
+  } else {
+    // Web Audio API is not available. Ask the user to use a supported browser.
+    alert("Oh nos! It appears your browser does not support the Web Audio API, please upgrade or use a different browser");
+  }
+
+  // variables
+  var analyserNode,
+      frequencyData = new Uint8Array(256);
+  const screen = document.querySelector('#screen'),
+      allRepeatedEls = document.querySelectorAll('#screen section'),
+      totalEls = allRepeatedEls.length;
+
+  // create an audio API analyser node and connect to source
+  function createAnalyserNode(audioSource) {
+    analyserNode = audioAPI.createAnalyser();
+    analyserNode.fftSize = 512;
+    audioSource.connect(analyserNode);
+  }
+
+  // get's html elements, loops over them & attaches a frequency from analysed data - what you do is up to you!
+  function animateStuff() {
+    analyserNode.getByteFrequencyData(frequencyData);
+
+    for (let i=0; i<totalEls; i++) {
+
+      var freqVol = frequencyData[i*4]/2;
+      allRepeatedEls[i].style.height = freqVol+'vh';
+    }
+    if (window.pause) { return };
+    requestAnimationFrame(animateStuff);
+
+  }
+
+  // getUserMedia success callback -> pipe audio stream into audio API
+  var gotStream = function(stream) {
+    // Create an audio input from the stream.
+    var audioSource = audioAPI.createMediaStreamSource(stream);
+    createAnalyserNode(audioSource);
+    animateStuff();
+  }
+
+  // pipe in analysing to getUserMedia
+  navigator.mediaDevices.getUserMedia({ audio: true, video: false })
+    .then(gotStream);
+
+
+  // hide and show animation
+  $(window).click(function() {
+    $('#screen').hide()
+    $('#screen').toggle();
+});
+
+
+
+}
